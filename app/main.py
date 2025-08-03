52618d8fa6e82d1967ad5ced9a0a9ab692485e9b
Слепцов Денис <d.sleptsov@comagic.dev>
2025-08-03 09:50:18 +0000

    Create bot

--- app/main.py
+++ app/main.py
@@ -1,17 +1,16 @@
 from aiogram import Bot, Dispatcher
 
 from app.config import AppConfig
+from app.factory import create_bot  # , create_dispatcher
 
 # from app.runners.app import run_polling, run_webhook
 from app.utils.logging import setup_logger
-
-# from app.factory import create_bot, create_dispatcher
 
 
 def main() -> None:
     setup_logger()
     config = AppConfig()  # type: ignore
-    # bot: Bot = create_bot(config=config)
+    bot: Bot = create_bot(config=config)
     # dispatcher: Dispatcher = create_dispatcher(config=config)
     # if config.telegram.use_webhook:
     #    return run_webhook(dispatcher=dispatcher, bot=bot, config=config)
